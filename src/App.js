5bb2a917b577d636145e28a8b7b163b30c39fe39
Yoon Kim <jyk416@ic.ac.uk>
2018-10-16 17:24:50 +0100

    Add in travis CI

--- src/App.js
+++ src/App.js
@@ -26,30 +26,13 @@ const styles = {
 
 function SimpleCard(props) {
   const { classes } = props;
-  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <Card className={classes.card}>
       <CardContent>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-          Word of the Day
-        </Typography>
-        <Typography variant="h5" component="h2">
-          be
-          {bull}
-          nev
-          {bull}o{bull}
-          lent
-        </Typography>
-        <Typography className={classes.pos} color="textSecondary">
-          adjective
-        </Typography>
-        <Typography component="p">
-          well meaning and kindly.
-          <br />
-          {'"a benevolent smile"'}
-        </Typography>
+        document.write("Card Content");
       </CardContent>
+      
       <CardActions>
         <Button size="small">Learn More</Button>
       </CardActions>
