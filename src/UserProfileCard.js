317f7ee6a62ca5774b4c8ad63f19a87f1f0e7b5f
Ece Kayan <ecekayan97@gmail.com>
2018-10-31 16:36:10 +0000

    Implemented user profile card layout

--- src/UserProfileCard.js
+++ src/UserProfileCard.js
@@ -1,29 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import Icon from '@material-ui/core/Icon';
 
 const styles = {
-  card: {
-    maxWidth: 345,
+	card: {
+	  maxWidth: '40%',
+	},
+  row: {
+    display: 'flex',
+    //justifyContent: 'center',
   },
-  media: {
-    height: 140,
+  avatar: {
+    width: 90,
+    height: 90,
+    margin: 10,
   },
-
-};
+	details: {
+	  margin: '10px',
+    
+	}
+ };
 
 
 class UserProfileCard extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+			accountID: 8349573,
+			balance: 9.765,
+			userName: 'John Doe',
     };
   }
 
@@ -31,28 +43,29 @@ class UserProfileCard extends React.Component {
     const { classes } = this.props;
     return (
       <Card className={classes.card}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              User Name
-            </Typography>
-          </CardContent>
-        </CardActionArea>
-        <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-      </Card>
-    );
+		    <CardContent>
+		     <div className={classes.row}>
+					 <Avatar
+					   src={require('./user_profile.png')}
+					   className={classes.avatar}
+					 />
+					 <div className={classes.details}>
+		         <Typography variant="h6" component="h2">
+					     {this.state.userName}   
+				     </Typography>
+					   <Typography className={classes.title} color="textSecondary" gutterBottom> Account ID: {this.state.accountID} </Typography>
+		         <Typography className={classes.pos} color="textSecondary"> Account Balance: {this.state.balance} </Typography>
+				   </div>
+				 </div>
+		    </CardContent>
+		    <CardActions>
+		      <Button size="small">
+					  Edit Profile
+					  <Icon>edit_icon</Icon>
+					</Button>
+		    </CardActions>
+			</Card>
+	  );
   }
 }
 
