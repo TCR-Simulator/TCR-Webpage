d8ce9398317ffe191be9331f4d1dbdc8225b723b
Matthew Cheung <cheung.kalong@gmail.com>
2018-10-31 17:21:30 +0000

    Added Profile menu button

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
 import teal from '@material-ui/core/colors/teal';
 import SettingsFrame from './SettingsFrame';
-
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import IconButton from'@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle'
 
 const theme = createMuiTheme({
   palette: {
@@ -24,29 +27,78 @@ const styles = {
   content: {
     height: '100%',
   },
+  grow: {
+    flexGrow: 1,
+  },
   settingFrame: {
     height: '100%',
     overflow: 'auto',
     zIndex: 10,
   },
-
-
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  }
 };
 
-const Dashboard = (props) => {
-  const { classes } = props;
-  return (
+class Dashboard extends React.Component {
+  state = {
+    anchorEl: null,
+  };
+
+  handleMenu = event => {
+    this.setState({anchorEl: event.currentTarget });
+  }
+
+  handleClose = () => {
+    this.setState({anchorEl: null});
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+    return (
       <div className={classes.root}>
         <AppBar position="relative" color="primary" elevation={2}>
           <Toolbar>
-            <Typography variant="h5" color="inherit">
+            <Typography variant="h5" color="inherit" className={classes.grow}>
               TCR Simulator
             </Typography>
+            <div>
+              <IconButton
+                aria-owns={open ? 'menu-appbar' : null}
+                aria-haspopup="true"
+                onClick={this.handleMenu}
+                color="inherit"
+              >
+              <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorE1={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                getContentAnchorEl={null}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={this.handleClose}
+              >
+                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+              </Menu>
+            </div>
           </Toolbar>
         </AppBar>
           <SettingsFrame className={classes.settingFrame} />
       </div>
-  );
+    );  
+  }
 };
 
 Dashboard.propTypes = {
