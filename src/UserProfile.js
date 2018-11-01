804afc4d609a1e7839b95ac77de7be9406fbcc28
hailey <hyf16@ic.ac.uk>
2018-11-01 23:09:57 +0000

    Fixed one more lint error

--- src/UserProfile.js
+++ src/UserProfile.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
 import UserProfileCard from './UserProfileCard';
 import UserDashboard from './UserDashboard';
 
