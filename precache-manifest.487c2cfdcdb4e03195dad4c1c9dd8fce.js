7e5cf96694a86b30dd4eaffbbcaf9218c70687c3
Yoon Kim <jyk416@ic.ac.uk>
2018-11-01 09:50:09 +0000

    Updates

---
+++ precache-manifest.487c2cfdcdb4e03195dad4c1c9dd8fce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0348170f84fbf1faa9eb",
+    "url": "/static/js/main.0348170f.chunk.js"
+  },
+  {
+    "revision": "c93cb18d1e69e22f0960",
+    "url": "/static/js/1.c93cb18d.chunk.js"
+  },
+  {
+    "revision": "0348170f84fbf1faa9eb",
+    "url": "/static/css/main.83f4ad38.chunk.css"
+  },
+  {
+    "revision": "1257aecc62cc6840372e12fa87fcb389",
+    "url": "/index.html"
+  }
+];
