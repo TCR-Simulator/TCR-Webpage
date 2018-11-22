e7eefcc37b945407ff835bd71a29b98759399d1f
Matthew Cheung <klc116@ic.ac.uk>
2018-11-22 10:51:52 +0000

    Added Token service api

---
+++ src/api/TokenService.js
@@ -0,0 +1,47 @@
+import { getToken } from '../config';
+
+export default class TokenService {
+  constructor() {
+    this.web3 = null;
+    this.token = null;
+  }
+
+  async init(web3) {
+    this.web3 = web3;
+    this.token = await getToken(this.web3);
+    window.token = this;
+  }
+
+  async balanceOf(account) {
+    return new Promise((resolve, reject) => {
+      this.token.balanceOf(account, (error, tx) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(tx);
+      });
+    });
+  }
+
+  async currentAccountBalance() {
+    return new Promise((resolve, reject) => {
+      this.token.currentAccountBalance((error, result) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(result);
+      });
+    });
+  }
+
+  async sendToken(to, value) {
+    return new Promise((resolve, reject) => {
+      this.token.sendToken(to, value, (error, tx) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(tx);
+      });
+    });
+  }
+}
