15a2f885a1764c0f05d36251d1d26901878de34d
Matthew Cheung <klc116@ic.ac.uk>
2018-11-22 15:54:48 +0000

    added getToken import from config

--- src/components/TokenBalance.js
+++ src/components/TokenBalance.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import { getToken } from '../config';
 
 export default class TokenBalance extends React.Component {
   state = {
