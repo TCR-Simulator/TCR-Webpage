a4c2622553a760d7a999cf9a414466f3ff2c22f1
Noel Lee <leekayau@gmail.com>
2018-12-23 02:02:08 +0800

    Use actual data for accepted list

--- src/AcceptedList.js
+++ src/AcceptedList.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
 import Done from '@material-ui/icons/Done';
 import Avatar from '@material-ui/core/Avatar';
-import Button from '@material-ui/core/Button';
+import ListingItem from './api/ListingItem';
 
 const styles = theme => ({
   root: {
@@ -43,87 +42,28 @@ const styles = theme => ({
   },
 });
 
-class AcceptedList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      checked: [],
-      items: ['Blank Space - Taylor Swift', 'New Rules - Dua Lipa', 'There is nothing holding me back - Shawn Mendes', 'Dive - Ed Sheeran', 'New Man - Ed Sheeran', 'Work - Rihanna'],
-    };
-  }
-
-  getChallengeButton() {
-    const { classes, type } = this.props;
-    if (type !== 'profile') {
-      return (
-        <Button
-          variant="outlined"
-          color="secondary"
-          className={classes.challengebutton}
-        >
-            Challenge
-        </Button>
-      );
-    }
-    return (<div />);
-  }
-
-  handleToggle = value => () => {
-    const { checked } = this.state;
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    this.setState({
-      checked: newChecked,
-    });
-  };
-
-
-  addItem(event) {
-    const currentItems = this.state.items; // eslint-disable-line react/destructuring-assignment
-    const textBox = event.target.previousElementSibling;
-
-    if (textBox.value) {
-      currentItems.push(textBox.value);
-      textBox.value = '';
-
-      this.setState({
-        items: currentItems,
-      });
-    }
-  }
-
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <List id="AcceptedList">
-          {this.state.items.map(value => ( // eslint-disable-line react/destructuring-assignment
-            <ListItem key={value} dense button>
-              <Avatar className={classes.avatar}>
-                <Done />
-              </Avatar>
-              <ListItemText primary={`${value}`} />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    );
-  }
-}
-
+const AcceptedList = ({ classes, listings }) => (
+  <div className={classes.root}>
+    <List id="AcceptedList">
+      {listings.map(({ listingHash, name, artist, url }) => (
+        <ListItem key={listingHash} dense button>
+          <Avatar className={classes.avatar}>
+            <Done />
+          </Avatar>
+          <ListItemText primary={`${name} - ${artist} (${url})`} />
+        </ListItem>
+      ))}
+    </List>
+  </div>
+);
 
 AcceptedList.propTypes = {
+  listings: PropTypes.arrayOf(PropTypes.instanceOf(ListingItem)),
   classes: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
-  type: PropTypes.string.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+
+AcceptedList.defaultProps = {
+  listings: [],
 };
 
 export default withStyles(styles)(AcceptedList);
